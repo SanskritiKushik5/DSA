93faf31b0e05a3336fce8d97cbba74553a6a5acf
Sanskriti Kushik <sanskritikushik23@gmail.com>
2025-09-06 11:28:13 +0530

    Update move-zeroes.cpp

--- Arrays/Easy/move-zeroes.cpp
+++ Arrays/Easy/move-zeroes.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        int j = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]!=0) {
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+    }
+};
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
         int count = 0;
         int n = nums.size();
         for(int i = 0; i<n; i++) {
